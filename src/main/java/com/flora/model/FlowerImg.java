b009c3860e338053444de75ecc7c1315684663ed
kakalun; <liqilun@126.com>
2016-12-12 23:11:10 +0800

    img

---
+++ src/main/java/com/flora/model/FlowerImg.java
@@ -0,0 +1,64 @@
+package com.flora.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class FlowerImg extends BaseObject{
+	private static final long serialVersionUID = 7484876054031890395L;
+	private Integer id;
+	private String imgName;
+	private Integer flowerId;
+	private Integer sortNum;
+	private String  tag;
+	private Date createTime;
+	public FlowerImg(){
+		
+	}
+	public FlowerImg(Integer flowerId, String tag, String imgName){
+		this.flowerId = flowerId;
+		this.tag = tag;
+		this.imgName = imgName;
+		this.sortNum = 1;
+		this.createTime = new Date();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getImgName() {
+		return imgName;
+	}
+	public void setImgName(String imgName) {
+		this.imgName = imgName;
+	}
+	public Integer getFlowerId() {
+		return flowerId;
+	}
+	public void setFlowerId(Integer flowerId) {
+		this.flowerId = flowerId;
+	}
+	public String getTag() {
+		return tag;
+	}
+	public void setTag(String tag) {
+		this.tag = tag;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	@Override
+	public Serializable realId() {
+		return id;
+	}
+	public Integer getSortNum() {
+		return sortNum;
+	}
+	public void setSortNum(Integer sortNum) {
+		this.sortNum = sortNum;
+	}
+}
--- src/main/java/com/flora/web/action/FlowerController.java
+++ src/main/java/com/flora/web/action/FlowerController.java
@@ -3,9 +3,13 @@ package com.flora.web.action;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.flora.Config;
 import com.flora.model.Flower;
+import com.flora.model.FlowerImg;
 import com.flora.util.BindUtils;
 import com.flora.util.StringUtil;
 import com.flora.web.util.WebUtils;
@@ -22,21 +27,45 @@ import com.flora.web.util.WebUtils;
 @Controller
 public class FlowerController extends BaseController {
 	@RequestMapping("/admin/flower/list.xhtml")
-	public String list(ModelMap model, HttpServletRequest request) {
-		Flower f = daoService.getObject(Flower.class, 1);
+	public String list(ModelMap model) {
+		List<Flower> flowerList = daoService.getObjectList(Flower.class, "id", false, 0, 100);
+		model.put("flowerList",flowerList);
 		return "admin/flower/list.vm";
 	}
 
 	@RequestMapping("/admin/flower/modify.xhtml")
-	public String modify(Integer id, ModelMap model, HttpServletRequest request) {
+	public String modify(Integer id, ModelMap model) {
 		if (id != null) {
 			model.put("flower", daoService.getObject(Flower.class, id));
+			List<FlowerImg> imgList = daoService.getObjectListByField(FlowerImg.class, "flowerId", id);
+			Map<String, FlowerImg> imgMap = new HashMap<String, FlowerImg>();
+			for(FlowerImg img : imgList){
+				imgMap.put(img.getTag(), img);
+			}
+			model.put("imgMap",imgMap);
 		}
 		return "admin/flower/modifyFlower.vm";
 	}
-
+	@RequestMapping("/admin/flower/flowerImgList.xhtml")
+	public String flowerImgList(Integer flowerId, ModelMap model) {
+		List<FlowerImg> imgList = daoService.getObjectListByField(FlowerImg.class, "flowerId", flowerId);
+		Map<String, FlowerImg> imgMap = new HashMap<String, FlowerImg>();
+		for(FlowerImg img : imgList){
+			imgMap.put(img.getTag(), img);
+		}
+		model.put("imgMap",imgMap);
+		return "admin/flower/flowerImgList.vm";
+	}
+	@RequestMapping("/admin/flower/setflowerImgSortNum.xhtml")
+	@ResponseBody
+	public String setflowerImgSortNum(Integer id, Integer sortNum, ModelMap model, HttpServletRequest request) {
+		FlowerImg img = daoService.getObject(FlowerImg.class, id);
+		img.setSortNum(sortNum);
+		daoService.saveObject(img);
+		return writeJsonSuccess(model);
+	}
 	@RequestMapping("/admin/flower/saveFlower.xhtml")
-	public String place(Integer id,  ModelMap model, HttpServletRequest request) {
+	public String place(Integer id,   ModelMap model, HttpServletRequest request) {
 		Flower flower = new Flower();
 		if(id!=null){
 			flower = daoService.getObject(Flower.class, id);
@@ -45,7 +74,23 @@ public class FlowerController extends BaseController {
 			flower.setAddTime(new Date());
 		}
 		BindUtils.bindData(flower, WebUtils.getRequestMap(request));
+		if(StringUtils.isBlank(flower.getName())){
+			return writeJsonError(model, "请输入植物的中文名称");
+		}
+		if(StringUtils.isBlank(flower.getContent())){
+			return writeJsonError(model, "请输入植物的简介");
+		}
 		daoService.saveObject(flower);
+		if(id!=null){
+			daoService.removeObjectList(daoService.getObjectListByField(FlowerImg.class, "flowerId", id));
+		}
+		daoService.saveObject(new FlowerImg(flower.getId(), "logo", flower.getLogo()));
+		for(int i=1;i<10;i++){
+			String img = request.getParameter("img"+i);
+			if(StringUtils.isNotBlank(img)){
+				daoService.saveObject(new FlowerImg(flower.getId(), "img"+i, img));
+			}
+		}
 		return writeJsonSuccess(model);
 	}
 
